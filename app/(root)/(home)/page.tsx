446306b48f464df38999e50b13bf999186dbf0de
Majid Ali <majidwp69.gmail.com>
2024-02-15 11:17:42 +0500

    create filter section

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,8 @@
+import HomeFilter from "@/components/home/HomeFilter";
+import Filter from "@/components/shared/Filter";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
 export default function Home() {
@@ -23,8 +26,14 @@ export default function Home() {
           placeholder="search for question"
           otherClasses="flex-1"
         />
-        Filter
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
+
+      <HomeFilter/>
     </>
   );
 }
