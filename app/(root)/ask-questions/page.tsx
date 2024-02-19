732cadaecdea864927d48baa015366f48e0622b8
Majid Ali <majidwp69.gmail.com>
2024-02-19 15:56:10 +0500

    create ask-question page

--- app/(root)/ask-questions/page.tsx
+++ app/(root)/ask-questions/page.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import Question from "@/components/forms/Question";
+import React from "react";
 
 const page = () => {
   return (
     <div>
-      <h2>Ask any questions</h2>
+      <h1 className="h1-bold text-dark100_light900">Ask a qusetion</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
