e73280667b05096fb3e5dae0553240c63fad7fad
Majid Ali <majidwp69.gmail.com>
2024-02-19 12:12:15 +0500

    update home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,46 @@
+import QuestionCard from "@/components/cards/QuestionCard";
 import HomeFilter from "@/components/home/HomeFilter";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
+
+const questions = [
+  {
+    _id: "1",
+    title: 'Cascading Deletes in SQLAlchemy?',
+    tags: [{ _id: '1', name: 'python' }, { _id: '2', name:
+      'sql' }],
+    author: {
+      _id: "1",
+      name: 'John Doe',
+      picture: 'john-doe.jpg',
+    },
+    upvotes: 1500,
+    views: 10000000,
+    answers: [],
+    createdAt: new Date('2021-09-01T12:00:00.000Z'),
+  },
+  {
+    
+    _id: "2",
+    title: 'How to center a div',
+    tags: [{ _id: '3', name: 'css' }, { _id: '4', name:
+      'html' }],
+    author: {
+      _id: "2",
+      name: 'John Doe',
+      picture: 'john-doe.jpg',
+    },
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date('2023-09-01T12:00:00.000Z'),
+  },
+];
+
 
 export default function Home() {
   return (
@@ -33,7 +70,33 @@ export default function Home() {
         />
       </div>
 
-      <HomeFilter/>
+      <HomeFilter />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {/* Looping through questions */}
+        {questions.length > 0 ? (
+          questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no question to show"
+            description=" Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. Your query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,41 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getTimeStamp = (createdAt: Date): string => {
+  const now = new Date();
+  const seconds = Math.floor((now.getTime() - createdAt.getTime()) / 1000);
+  let interval = seconds / 31536000; // Number of seconds in a year
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years ago";
+  }
+  interval = seconds / 2592000; // Number of seconds in a month
+  if (interval > 1) {
+    return Math.floor(interval) + " months ago";
+  }
+  interval = seconds / 86400; // Number of seconds in a day
+  if (interval > 1) {
+    return Math.floor(interval) + " days ago";
+  }
+  interval = seconds / 3600; // Number of seconds in an hour
+  if (interval > 1) {
+    return Math.floor(interval) + " hours ago";
+  }
+  interval = seconds / 60; // Number of seconds in a minute
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+};
+
+
+export const formatNumber = (num: number): string => {
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(2) + "M"; // Divides by 1,000,000 and appends "M"
+  } else if (num >= 1000) {
+    return (num / 1000).toFixed(2) + "K"; // Divides by 1,000 and appends "K"
+  } else {
+    return num.toString(); // Returns the number as a string if less than 1,000
+  }
+};
