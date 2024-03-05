5ab15189b4341682ced20106b66aac4fd765021c
Majid Ali <majidwp69.gmail.com>
2024-03-05 10:29:49 +0500

    add user image

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -53,15 +53,16 @@ const QuestionCard = ({
           <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
         ))}
       </div>
-      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+      <div className="flex-between mt-6 w-full flex-wrap gap-3 ">
         <Metric
-          imgUrl="/assets/icons/avatar.svg"
+          imgUrl={author?.picture}
           alt="user"
           value={author?.name}
           title={` - asked ${getTimeStamp(createdAt)}`}
           href={`/profile/${author?._id}`}
           isAuthor
-          textStyles="body-medium text-dark400_light700"
+          textStyles="body-medium text-dark400_light700 "
+          
         />
         <Metric
           imgUrl="/assets/icons/like.svg"
